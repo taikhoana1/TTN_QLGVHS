b45b16ef92613729c18eb08298b1014e31cdd859
taikhoana1 <noidovatoi11196@gmail.com>
2017-10-09 21:31:44 +0700

    formmain

--- Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/Main.cs
+++ Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/Main.cs
@@ -13,9 +13,37 @@ namespace QUANLY_HS_GV
 {
     public partial class frmMain : Form
     {
+        public partial class frmMain : Form
+    {
         public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnhuongdan_Click(object sender, EventArgs e)
+        {
+            huongdan hd = new huongdan();
+            hd.ShowDialog();
+        }
+
+        private void btnhocsinh_Click(object sender, EventArgs e)
+        {
+            HocSinh hs = new HocSinh();
+            hs.ShowDialog();
+        }
+
+        private void btngiaovien_Click(object sender, EventArgs e)
+        {
+            GiaoVien gv = new GiaoVien();
+            gv.ShowDialog();
         }
         }
 }
