48ef4c99d2b4d95e734c7ca20e925f76b71bc34b
taikhoana1 <noidovatoi11196@gmail.com>
2017-10-09 21:00:15 +0700

    formHD

--- Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/huongdan.cs
+++ Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/huongdan.cs
@@ -12,9 +12,30 @@ namespace QUANLY_HS_GV
 {
     public partial class huongdan : Form
     {
+
         public huongdan()
         {
             InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string str;
+            if (treeView1.SelectedNode.Name == "nhomthuchien")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Nhom.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "gioithieusanpham")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Gioi Thieu.mht";
+                webBrowser1.Navigate(str);
+            }
+            if (treeView1.SelectedNode.Name == "hdchinh")
+            {
+                str = Application.StartupPath + "\\Huong Dan\\Hocsinh_Giaovien.mht";
+                webBrowser1.Navigate(str);
+            }
         }
 
        
