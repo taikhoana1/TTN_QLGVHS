65da3005b5f8d6f974eb59f75646061d07f3cf5c
windatm <tuanphongmta0205@gmail.com>
2017-10-09 20:11:31 +0700

    FrmHócinh1

--- Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/HocSinh.cs
+++ Quản lý học sinh viên/QUANLY_HS_GV/QUANLY_HS_GV/HocSinh.cs
@@ -18,5 +18,18 @@ namespace QUANLY_HS_GV
         {
             InitializeComponent();
         }
+
+        bool check = true;
+
+        private void HocSinh_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'qUANLY_HS_GVDataSet.HocSinh' table. You can move, or remove it, as needed.
+            //this.hocSinhTableAdapter.Fill(this.qUANLY_HS_GVDataSet.HocSinh);
+            // TODO: This line of code loads data into the 'qUANLY_HS_GVDataSet1.HocSinh' table. You can move, or remove it, as needed.
+            //this.hocSinhTableAdapter1.Fill(this.qUANLY_HS_GVDataSet1.HocSinh);
+            // TODO: This line of code loads data into the 'qUANLY_HS_GVDataSet.HocSinh' table. You can move, or remove it, as needed.
+            //this.hocSinhTableAdapter.Fill(this.qUANLY_HS_GVDataSet.HocSinh);
+
+        }
     }
 }
